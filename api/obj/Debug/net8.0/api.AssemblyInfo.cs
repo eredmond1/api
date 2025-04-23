d0131fbd1a494650115459ede02bfa5d0a76ef10
eredmond1 <150072405+eredmond1@users.noreply.github.com>
2025-04-22 22:20:13 -0600

    update_comments

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92c0efe48e02b68311c8b8aab570ed5549ee6f83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1db82f8fb5ff638771cc7870fbd4dc751a04bec7")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
